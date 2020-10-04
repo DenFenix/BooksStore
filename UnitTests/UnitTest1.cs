6684d25ff7ab8160055346bf65330dd598dc8c93
Denis M <denis.m.a.3@gmail.com>
2020-10-05 02:37:30 +0300

    Добавил Пангинацию и юнит тесты

--- UnitTests/UnitTest1.cs
+++ UnitTests/UnitTest1.cs
@@ -1,5 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Domain.Abstract;
+using Domain.Entities;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using WebUI.Controllers;
 
 namespace UnitTests
 {
@@ -7,8 +13,28 @@ namespace UnitTests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CanPaginate()
         {
+            Mock<IBookRepository> mock = new Mock<IBookRepository>();
+            mock.Setup(m => m.Books).Returns(new List<Book>()
+            {
+                 new Book {BookId = 1, Name  = "Книга1"},
+                 new Book {BookId = 2, Name  = "Книга2"},
+                 new Book {BookId = 3, Name  = "Книга3"},
+                 new Book {BookId = 4, Name  = "Книга4"},
+                 new Book {BookId = 5, Name  = "Книга5"},
+                 new Book {BookId = 6, Name  = "Книга6"}
+            });
+            BookController controller = new BookController(mock.Object);
+            controller.pageSize = 2;
+
+            IEnumerable<Book> result = (IEnumerable<Book>)controller.List(2).Model;
+
+            var books = result.ToList();
+            Assert.IsTrue(books.Count == 2);
+            Assert.AreEqual(books[0].BookId, 3);
+            Assert.AreEqual(books[1].BookId, 4);
+
         }
     }
 }
--- WebUI/Controllers/BookController.cs
+++ WebUI/Controllers/BookController.cs
@@ -10,15 +10,20 @@ namespace WebUI.Controllers
     public class BookController : Controller
     {
         private IBookRepository repository;
+        //на одной странице 5 товаров
+        public int pageSize = 3;
 
         public BookController(IBookRepository repository)
         {
             this.repository = repository;
         }
 
-        public ViewResult List()
+        public ViewResult List(int page = 1)
         {
-            return View(repository.Books);
+            return View(repository.Books
+                .OrderBy(book=>book.BookId)
+                .Skip((page-1)*pageSize)
+                .Take(pageSize));
         }
     }
 }
