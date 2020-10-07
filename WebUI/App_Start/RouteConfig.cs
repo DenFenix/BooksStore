82fee0113d2bbc25b11bc97ad75d4f48b585c695
Denis M <denis.m.a.3@gmail.com>
2020-10-07 17:38:00 +0300

    Изменение схемы url

--- WebUI/App_Start/RouteConfig.cs
+++ WebUI/App_Start/RouteConfig.cs
@@ -13,16 +13,43 @@ namespace WebUI
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: null,
-                url: "Page{page}",
-                defaults: new { controller = "Book", action="List"});
+            //routes.MapRoute(
+            //    name: null,
+            //    url: "Page{page}",
+            //    defaults: new { controller = "Book", action="List"});
+
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Book", action = "List", id = UrlParameter.Optional }
+            //);
+
+            routes.MapRoute(null,
+                "",
+                new
+                {
+                    controller = "Book",
+                    action = "List",
+                    categoru = (string)null,
+                    page = 1
+                });
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Book", action = "List", id = UrlParameter.Optional }
+                name:null,
+                url:"Page{page}",
+                defaults: new { controller = "Book", action = "List" },
+                constraints: new { page = @"\d+" });
+
+            routes.MapRoute(null, "{category}",
+                new { controller = "Book", action = "List", page = 1 });
+
+            routes.MapRoute(null,
+                "{category}/Page{page}",
+                new { controller = "Book", action = "List" },
+                new { page = @"\d+" }
             );
+
+            routes.MapRoute(null, "{controller}/{action}");
         }
     }
 }
