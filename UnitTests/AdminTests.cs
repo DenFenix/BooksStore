19d511639cf6e5ccb7538b19fdd2206b4f336694
Denis M <denis.m.a.3@gmail.com>
2020-10-12 14:52:02 +0300

    Админ панель: список товаров

---
+++ UnitTests/AdminTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Domain.Abstract;
+using Domain.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using WebUI.Controllers;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class AdminTests
+    {
+        [TestMethod]
+        public void Index_Contains_All_Games()
+        {
+            Mock<IBookRepository> mock = new Mock<IBookRepository>();
+            mock.Setup(x=>x.Books).Returns(new List<Book>
+            {
+                new Book {BookId =1, Name = "Book1"},
+                new Book {BookId =2, Name = "Book2"},
+                new Book {BookId =3, Name = "Book3"},
+                new Book {BookId =4, Name = "Book4"}
+            });
+
+            // Организация - создание контроллера
+            AdminController controller = new AdminController(mock.Object);
+
+            //Действие
+            List<Book> result = ((IEnumerable<Book>)controller.Index().ViewData.Model).ToList();
+
+            Assert.AreEqual(result.Count, 4);
+            Assert.AreEqual("Book1", result[0].Name);
+            Assert.AreEqual("Book2", result[1].Name);
+            Assert.AreEqual("Book3", result[2].Name);
+        }
+    }
+}
---
+++ WebUI/Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        IBookRepository repository;
+
+        public AdminController (IBookRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult Index()
+        {
+            return View(repository.Books);
+        }
+    }
+}
